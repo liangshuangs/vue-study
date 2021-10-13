e6bb7b3d1ee3135017abe8e94c496f616f317083
liangshuangs <liangshuang15@baidu.com>
2021-10-13 11:02:08 +0800

    vue3 init

---
+++ study-vue3/rollup.config.js
@@ -0,0 +1,17 @@
+import babel from 'rollup-plugin-babel';
+import resolve from 'rollup-plugin-node-resolve';
+export default {
+    input: 'src/main.js',
+    output: {
+        file: 'dist/bundle.js',
+        name: 'Vue',
+        format: 'umd', //兼容 规范 script导入 amd commonjs
+        sourceMap: true
+    },
+    plugins:[
+        resolve(),
+        babel({
+            exclude:'node_modules/**'
+        })
+    ]
+}
